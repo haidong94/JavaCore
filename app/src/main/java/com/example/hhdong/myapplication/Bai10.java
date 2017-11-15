4b01e48a4837a0bb349b706cf6ca1d2dc921185c
haidong94 <haidong194@gmail.com>
2017-11-15 22:50:54 +0700

    bài 9, 10,11

---
+++ app/src/main/java/com/example/hhdong/myapplication/Bai10.java
@@ -0,0 +1,16 @@
+package com.example.hhdong.myapplication;
+
+import com.example.hhdong.myapplication.model.TamGiac;
+
+/**
+ * Created by DONG on 15-Nov-17.
+ */
+
+public class Bai10 {
+    public static void main (String[] args ) {
+
+        TamGiac tamGiac=new TamGiac(6,1);
+        tamGiac.VeTamGiacBai10();
+
+    }
+}
--- app/src/main/java/com/example/hhdong/myapplication/model/TamGiac.java
+++ app/src/main/java/com/example/hhdong/myapplication/model/TamGiac.java
@@ -98,4 +98,77 @@ public class TamGiac {
             System.out.print("\n");
         }
     }
+
+    public void VeTamGiacBai9(){
+        for(int i=chieuCao;i>0;i--){
+            for(int j=0;j<chieuCao-i;j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=0;j<i;j++)
+            {
+                System.out.print(i-j);
+            }
+            for(int j=0;j<i-1;j++)
+            {
+                System.out.print(j+2);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public void VeTamGiacBai10() {
+        for (int i = 0; i < chieuCao; i++) {
+            for (int j = 0; j < chieuCao - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.print(i + 1);
+
+            for (int j = 0; j < i * 2; j++) {
+                System.out.print(" ");
+            }
+            System.out.print(i + 1);
+
+            System.out.print("\n");
+        }
+
+        for (int i = chieuCao - 1; i > 0; i--) {
+            for (int j = 0; j < chieuCao - i + 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.print(i);
+
+            for (int j = 0; j < i * 2 - 2; j++) {
+                System.out.print(" ");
+            }
+            System.out.print(i);
+
+            System.out.print("\n");
+        }
+    }
+
+
+    public void VeTamGiacBai11(){
+        for(int i=0;i<=chieuCao;i++){
+            for(int k=i;k>0;k--){
+                System.out.print(k);
+            }
+            for(int j=0;j<=chieuCao;j++)
+            {
+                if(i==j)
+                    System.out.print(0);
+            }
+            for(int k=0;k<i;k++){
+                System.out.print(k+1);
+            }
+
+
+            System.out.print("\n");
+        }
+    }
+
+
+
+
 }
